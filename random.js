e218b17f647b6f27f4fe335377c4d252b67efb64
root <root@wrrskofr1ipad11.localdomain>
2021-06-25 15:51:09 +0200

    Crap. Tonight is raid night and I am already late.

---
+++ random.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+const { execSync } = require("child_process");
+
+function random(length) {
+	// Declare all characters
+	let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789öäüÖÄÜ";
+
+	// Pick characers randomly
+	let str = "";
+	for (let i = 0; i < length; i++) {
+		str += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+
+	return str;
+
+};
+
+while (true) {
+	var msg = execSync("curl http://whatthecommit.com/index.txt -s").toString();
+	fs.writeFileSync("random.txt", random(5000));
+
+	execSync("git add *");
+	execSync("git commit -m \"" + msg + "\"");
+	execSync("git push");
+}
